65432f38a377c35fa7c5308295d3a976d4dbf987
Rarma03 <raj0701verma@gmail.com>
2025-01-27 15:34:19 +0530

    fixes in homepage, added login & register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './components/Home/HomePage.jsx';
+import RegisterPage from './components/Register/RegisterPage.jsx';
+import LoginPage from './components/Login/LoginPage.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
+      <Route path='/register' element={<RegisterPage />}></Route>
+      <Route path='/login' element={<LoginPage />}></Route>
     </Routes>
   )
 }
