96d1d499c683a5198481e6e49ff82af3eb7354a4
Rarma03 <raj0701verma@gmail.com>
2025-02-04 21:23:56 +0530

    fix border in logo

--- client/src/components/MainNavBar/Adders/WebLogo.jsx
+++ client/src/components/MainNavBar/Adders/WebLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WebLogo = () => {
     return (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center bg-white rounded-full border-r-2 border-amber-500'>
             <div className='text-bold text-4xl'>GS-Campus</div>
             <div className='text-bold text-2xl'>---Connect---</div>
             {/* <img src="/images/website-logo.png" alt="" /> */}
