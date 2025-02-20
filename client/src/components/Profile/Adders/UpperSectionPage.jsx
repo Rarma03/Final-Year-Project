01f654dd5fb9f82be16c92e76284e08cf36ea2fe
Rarma03 <raj0701verma@gmail.com>
2025-02-20 15:48:39 +0530

    added logout button, Backend route for logout is not written yet

--- client/src/components/Profile/Adders/UpperSectionPage.jsx
+++ client/src/components/Profile/Adders/UpperSectionPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const UpperSectionPage = ({ user }) => {
     const userName = user.name;
@@ -36,7 +37,7 @@ const UpperSectionPage = ({ user }) => {
     return (
         <div className="flex flex-col md:flex-row bg-white p-4 border border-amber-400 rounded-tl-3xl rounded-tr-full shadow-md items-center md:items-start">
             {/* Profile Icon */}
-            <div className="flex justify-center md:justify-start w-full md:w-auto">
+            <Link to={'/profile'} className="flex justify-center md:justify-start w-full md:w-auto">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-20 w-20 md:h-[100px] md:w-[100px]">
                     <defs>
                         <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="100%">
@@ -46,7 +47,7 @@ const UpperSectionPage = ({ user }) => {
                     </defs>
                     <path fill="url(#gradient1)" strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
-            </div>
+            </Link>
 
             {/* Divider - Hidden on small screens */}
             <div className="hidden md:block border-l-2 border-white h-full mx-4"></div>
