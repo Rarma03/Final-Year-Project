6f42514d365d527e479b4a5c23fb76ec554b8d9f
Rarma03 <raj0701verma@gmail.com>
2025-03-13 17:02:50 +0530

    backend deployed succesfully, changed frontend Request header to deployed backend url

--- client/src/components/Profile/Adders/CreateFlatRequestForm.jsx
+++ client/src/components/Profile/Adders/CreateFlatRequestForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const CreateFlatRequestForm = () => {
     const { user } = useContext(AuthContext);
-    console.log(user);
+    // console.log(user);
     const navigate = useNavigate();
 
     const [formData, setFormData] = useState({
