f0ce60c8160640ef5226ef5b2594c0d3bf466a08
Rarma03 <raj0701verma@gmail.com>
2025-03-13 16:18:10 +0530

    chng-03

--- api/index.js
+++ api/index.js
@@ -36,7 +36,9 @@ app.get('/', (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+export default app;
